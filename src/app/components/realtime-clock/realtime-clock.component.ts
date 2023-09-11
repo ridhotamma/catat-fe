e91da2666b74d87e770c7adeef364c70d1ca2d06
ridhotamma <ridho.tamma@gmail.com>
2023-09-11 08:57:14 +0700

    feat: add date on welcome page

--- src/app/components/realtime-clock/realtime-clock.component.ts
+++ src/app/components/realtime-clock/realtime-clock.component.ts
@@ -11,6 +11,23 @@ export class RealtimeClockComponent implements OnInit {
 
   constructor() { }
 
+  
+  getDaySuffix(day: number): string {
+    if (day >= 11 && day <= 13) {
+      return 'th';
+    }
+    switch (day % 10) {
+      case 1:
+        return 'st';
+      case 2:
+        return 'nd';
+      case 3:
+        return 'rd';
+      default:
+        return 'th';
+    }
+  }
+  
   ngOnInit(): void {
     interval(1000).subscribe(() => {
       this.currentTime = new Date();
